6ff546baa54d82c94831be1b1e4847f77b29a7ed
Mateo071 <floresmateo2000@gmail.com>
2023-08-24 15:46:22 -0400

    displayed mock data

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 
-router.get('/', (req, res) => { 
-    res.send("users");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => { 
+    try {
+        const results = await prisma.user.findMany();
+        res.send(results); 
+    } catch (err) {
+        res.send(err);
+    }
+    
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const results = await prisma.user.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(results)
+    } catch (err) {
+        res.send(err);
+    }
+})
 
 module.exports = router;
--- server/api/watches.js
+++ server/api/watches.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 
-router.get('/', (req, res) => { 
-    res.send("watches");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => { 
+    try {
+        const results = await prisma.watches.findMany()
+        res.send(results); 
+    } catch (err) {
+        res.send(err);
+    }
+    
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const results = await prisma.watches.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(results)
+    } catch (err) {
+        res.send(err);
+    }
+})
 
 module.exports = router;
