64a9a38b261f6f5531597ac372558790cd7bf3d5
Sharul Sonthalia <sharulsonthalia@Sharuls-MacBook-Pro.local>
2023-08-29 20:56:04 +0100

    uncommented AboutUs in App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,5 +1,39 @@
+import { Route, Routes} from 'react-router-dom'
+import AboutUs from './pages/AboutUs'
+import Admin from './pages/Admin'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import LogIn from './pages/LogIn'
+import Profile from './pages/Profile'
+import Shop from './pages/Shop'
+import SignUp from './pages/SignUp'
+import SingleItem from './pages/SingleItem'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+
 const App = () => {
-  return <></>;
+  return (
+    <section>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<AboutUs />}/>
+          <Route path='/admin' element={<Admin />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/checkout' element={<Checkout />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/login' element={<LogIn />}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/shop' element={<Shop />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/singleitem' element={<SingleItem />}/>
+        </Routes>
+      <Footer />
+    </section>
+  );
 };
 
 export default App;
--- public/src/components/NavBar.jsx
+++ public/src/components/NavBar.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
+
 const NavBar = () => {
     return (
-        <>
-            <h1>NavBar</h1>
-        </>
+        <section>
+            <nav>
+                <Link className = "navLink" to = '/'> Hours and Minutes </Link>
+                <Link className = "navLink" to = '/shop?type=men'> Men's </Link>
+                <Link className = "navLink" to = '/shop?type=women'> Women's </Link>
+                <Link className = "navLink" to = '/shop?type=brand'> Shop by Brand </Link>
+                <Link className = "navLink" to = '/shop?type=featured'> Featured </Link>
+            </nav>        
+        </section>
     )
 }
 
