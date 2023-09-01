8e24b3a2695f6319a6d1a2e60a7119826855499e
boone84 <boone84@Boone-PC.localdomain>
2023-09-01 16:27:50 -0400

    style navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,57 +1,40 @@
 import { Link } from "react-router-dom";
+import './NavBar.css';
 
-const NavBar = ({setToken, token}) => {
+const NavBar = ({ setToken, token }) => {
   return (
     <header>
       <nav>
-        <Link className="navLink" to="/">
-          {" "}
-          H&M{" "}
-        </Link>
-        <Link className="navLink" to="/shop?type=men">
-          {" "}
-          Men{" "}
-        </Link>
-        <Link className="navLink" to="/shop?type=women">
-          {" "}
-          Women{" "}
-        </Link>
-        <Link className="navLink" to="/shop?type=brand">
-          {" "}
-          Shop by Brand{" "}
-        </Link>
-        <Link className="navLink" to="/shop?type=featured">
-          {" "}
-          Featured{" "}
-        </Link>
-        <Link className="navLink" to="/cart">
-          {" "}
-          Cart{" "}
-        </Link>
-        {token ? (
-          <div>
-            <Link className="navLink" to="/profile">
-              {" "}
-              Profile{" "}
-            </Link>
-            <Link
-              className="navLink"
-              to="/"
-              onClick={() => {
-                localStorage.removeItem("logintoken");
-                setToken("");
-              }}
-            >
-              {" "}
-              Sign Out{" "}
-            </Link>
-          </div>
-        ) : (
-            <Link className="navLink" to="/login" state={{ token: token }}>
-              {" "}
-              Login{" "}
-            </Link>
-        )}
+        <div className="nav-left">
+          <Link className="navLink" to="/">H&M</Link>
+          <Link className="navLink" to="/shop?type=men">Men</Link>
+          <Link className="navLink" to="/shop?type=women">Women</Link>
+          <Link className="navLink" to="/shop?type=brand">Brand</Link>
+          <Link className="navLink" to="/shop?type=featured">Featured</Link>
+        </div>
+        <div className="nav-center">
+        <Link className="navLink center-logo" to="/">H&M</Link>
+        </div>
+        <div className="nav-right">
+          <Link className="navLink" to="/cart">Cart</Link>
+          {token ? (
+            <>
+              <Link className="navLink" to="/profile">Profile</Link>
+              <Link
+                className="navLink"
+                to="/"
+                onClick={() => {
+                  localStorage.removeItem("logintoken");
+                  setToken("");
+                }}
+              >
+                Sign Out
+              </Link>
+            </>
+          ) : (
+            <Link className="navLink" to="/login" state={{ token: token }}>Login</Link>
+          )}
+        </div>
       </nav>
     </header>
   );
